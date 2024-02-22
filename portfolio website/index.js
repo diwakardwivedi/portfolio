6e15e781ee04eb6c5496c6f85ac2a73de664ba65
diwakar dwivedi <diwakardwivedi.624@gmail.com>
2024-02-22 11:47:36 +0530

    i added my html,css and js files and it's my first commit

---
+++ portfolio website/index.js
@@ -0,0 +1,6 @@
+let typed = new Typed('#element', {
+    strings: ['JAVASCRIPT', 'HTML', 'CSS', 'REACT JS', 'NODE JS', 'EXPRESS JS', 'MONGODB'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+});
